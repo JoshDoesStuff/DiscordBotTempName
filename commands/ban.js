e573a2c7a6f7c9d3bf148f071f1b1eb2a43f7014
Josh <JoshG4569@proton.me>
2023-01-07 12:12:05 -0800

    update ban and kick

--- commands/ban.js
+++ commands/ban.js
@@ -1,13 +1,20 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
-const data = new SlashCommandBuilder()
-	.setName('ban')
-	.setDescription('Select a member and ban them.')
-	.addUserOption(option =>
-		option
-			.setName('target')
-			.setDescription('The member to ban')
-			.setRequired(true))
-	.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
-	.setDMPermission(false);
-    Guild.BanMembers(option);
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ban')
+    .setDescription('Select a member and ban them.')
+    .addUserOption(option =>
+      option
+        .setName('target')
+        .setDescription('The member to ban')
+        .setRequired(true))
+    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+    .setDMPermission(false),
+
+  async execute(interaction) {
+    const target = interaction.options.getUser('target');
+    await interaction.reply(`Banned ${target.username}. That felt good.`);
+    await interaction.guild.members.ban(target);
+  },
+};
--- commands/kick.js
+++ commands/kick.js
@@ -1,11 +1,20 @@
 const { SlashCommandBuilder, PermissionFlagsBits } = require('discord.js');
 
-const data = new SlashCommandBuilder()
-	.setName('kick')
-	.setDescription('Select a member and kick them.')
-	.addUserOption(option =>
-		option
-			.setName('target')
-			.setDescription('The member to kick')
-			.setRequired(true))
-	.setDefaultMemberPermissions(PermissionFlagsBits.BanMembers | PermissionFlagsBits.KickMembers);
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('kick')
+    .setDescription('Select a member and kick them.')
+    .addUserOption(option =>
+      option
+        .setName('target')
+        .setDescription('The member to kick')
+        .setRequired(true))
+    .setDefaultMemberPermissions(PermissionFlagsBits.BanMembers)
+    .setDMPermission(false),
+
+  async execute(interaction) {
+    const target = interaction.options.getUser('target');
+    await interaction.reply(`Kicked ${target.username}. Maybe they will learn to read the rules.`);
+    await interaction.guild.members.kick(target);
+  },
+};
